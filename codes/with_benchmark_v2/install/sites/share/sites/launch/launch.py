78a86ae5ca16bdbe7d2c2c58958b64c53d8e7759
Vehshanaan@wsl2Ubuntu2204 <1959180242@qq.com>
2023-08-15 22:19:01 +0100

    写了一点算法描述，进行了一部分实验

--- codes/with_benchmark_v2/install/sites/share/sites/launch/launch.py
+++ codes/with_benchmark_v2/install/sites/share/sites/launch/launch.py
@@ -13,8 +13,8 @@ from launch_ros.actions import Node
 map_path = "/mnt/a/OneDrive/MScRobotics/Dissertation2022/codes/benchmarks/my_own_benchmarks/warehouse-10-20-10-2-1.png"
 scene_path = "/mnt/a/OneDrive/MScRobotics/Dissertation2022/codes/benchmarks/my_own_benchmarks/warehouse-10-20-10-2-1.pngJul241220.scen"
 
-frame_length = 70
-node_total = 40
+frame_length = 20
+node_total = 50
 required_length = frame_length # 每次要求生成计划的长度
 
 
